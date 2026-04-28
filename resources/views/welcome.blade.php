e3690d71357ff3afde256ac705eb575610342a3b
Anna1861 <its.all1861@gmail.com>
2026-04-28 10:39:07 +0200

    add logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
 }
 </style>
 
-<img src="{{ asset('images/logo.png') }}" alt="Logo">
+<img src="{{ asset('images/logo.jpg') }}" alt="Logo">
 <body class="main-bg text-light">
 <nav class="navbar navbar-expand-lg navbar-dark bg-warning mb-4">
     <div class="container-fluid">
